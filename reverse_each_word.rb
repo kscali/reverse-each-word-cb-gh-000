1239f40dbd5f243cfc8c6f47f1bf5283a472fbe5
Koy Saeteurn <kosatun@aol.com>
2018-07-26 01:32:17 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,9 @@
 def reverse_each_word(string)
-  word = string.split("").reverse.join("")
-  word
+  new_array = string.split
+  new_string = []
+  new_array.each do |word|
+    new_string << word.reverse
+  end 
+  new_string.join("")
+  new_string
 end  
